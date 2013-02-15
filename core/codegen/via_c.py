2a0694c3d022a2d7e21a2a07f95e6a9732a032a0
Matthew Sorensen <matthew.d.sorensen@gmail.com>
2013-02-14 16:41:08 -0800

    that isn't scary at all

--- core/codegen/via_c.py
+++ core/codegen/via_c.py
@@ -1,4 +1,3 @@
-
 import sympy.utilities.codegen as gen
 
 from sympy import symbols
@@ -6,26 +5,37 @@ from sympy.utilities.codegen import codegen
 from sympy.abc import x, y, z
 from StringIO import StringIO
 
+import pycuda.autoinit
+import pycuda.driver as drv
+import numpy
+from pycuda.compiler import SourceModule
+
 class CUDAGen(gen.CCodeGen):
     def get_prototype(self, routine):
         """ We must modify all prototypes to include __device__ """
         return "__device__ " + super(CUDAGen,self).get_prototype(routine)
 
     def _preprocessor_statements(self, prefix):
-        return ["#include <math.h>\n","#include \"cuda_runtime.h\"\n"]
-
+        return []
+    
     def write(self,routines, header = False, empty = True):
         contents = StringIO()
         self.dump_c(routines, contents, ".cu", header, empty)
-        return contents.getvalue()
+        return contents.getvalue() + self.boilerplate()
         
-# then we must write out a function that actually does all of the work
-"""
-__device__ kernel(float* out, int length, int width, float dx, float dy, float sx, float sy, z){
-    int x, int y;
-    float fx, float fy;
-    float sample = f(fx,fy,z);
-    out[y * length + x] = sample;
+    def boilerplate(self):
+        return """
+__global__ void kernel(float* out, int length, int width, float z){
+    const int x = blockIdx.x * blockDim.x + threadIdx.x;
+    const int y = blockIdx.y * blockDim.y + threadIdx.y;
+
+    if(length <= x || width <= y)
+        return;
+
+    const float fx = x / (float) length;
+    const float fy = y / (float) width;
+
+    out[y * length + x] = f(fx,fy,z);
 }
 
 """
@@ -37,5 +47,13 @@ def cudagen(expr):
     return code_gen.write(routines)
 
 def test():
-    code = cudagen(x + y * x / z)
-    print code
+    code = cudagen(x + y )
+    return code
+
+mod = SourceModule(test())
+
+f = mod.get_function("kernel")
+dest = numpy.zeros((10,10)).astype(numpy.float32)
+
+f(drv.Out(dest),numpy.int32(10),numpy.int32(10),numpy.float32(1), block=(10,10,1), grid=(1,1))
+print dest
