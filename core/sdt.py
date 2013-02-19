d6647bd6addcfa35359f3bd91a85119e17e543fe
Matthew Sorensen <matthew.d.sorensen@gmail.com>
2013-02-19 00:50:11 -0800

    Deal with zero as a real thing, not a positive number

--- core/sdt.py
+++ core/sdt.py
@@ -1,46 +1,52 @@
-
 import math
-from util import sgn
 
 def square(x):
     return x * x
 
-
-def reconstruct(coeff,size, old):
-    minx, maxx = coeff
-
-    if minx is None:
-        for i in range(0,maxx):
-            old[i] = math.copysign(square(i - maxx), old[i])
-    elif maxx is None:
-        for i in range(minx,size):          
-            old[i] = math.copysign(square(i - minx), old[i])
+def copysign(x,y):
+    if x == 0:
+        return 0
     else:
-        for i in range(minx,maxx+1):
-            old[i] = math.copysign(min([square(i - minx), square(i - maxx)]), old[i])
+        return math.copysign(x,y)
+
+def sgn(x):
+    if x == 0:
+        return 0
+    else:
+        return math.copysign(1,x)
 
 def first_pass(samples):
-    # we want to turn samples into a piece-wise set of quadratics
-    # coeffs contains (min, max) for each quadratic
     psgn = sgn(samples[0])
-    coeffs = [(None,None)]
+    last = None
 
-    for i, s in enumerate(samples):
+    for i, s in enumerate(samples[:]):
         ssgn = sgn(s)
         if ssgn != psgn:
-            # close the current coeffient and start a new one
-            oldmin, oldmax = coeffs[-1]
-            coeffs[-1] = (oldmin, i)
-            coeffs.append((i,None))
+            if psgn == 0:
+                i = i - 1
+            if last is None:
+                for x in range(0,i):
+                    samples[x] = copysign(square(i - x), samples[x])
+            else:
+                for x in range(last,i+1):
+                    # we can split this in two
+                    samples[x] = copysign(min([square(x -last),square(x - i)]), samples[x])
+            last = i
+        elif ssgn == 0: # by previous, psgn also is 0
+            last = i + 1
+
         psgn = ssgn
-        
-    # so now we reconstruct the sample as a bunch of distances
-    ls = len(samples)
 
-    for i, s in enumerate(samples):
-        samples[i] = math.copysign(ls * ls, s)
-
-    for c in coeffs:
-        reconstruct(c, ls, samples)
-
+    if last is None:
+        # this implies we have no sign changes, so we replace all the values
+        # with a value guaranteed to be changed in the next passs.
+        ls = square(len(samples))
+        for i, s in enumerate(samples[:]):
+            if s != 0:
+                samples[i] = copysign(ls, s)
+    else:
+        # Otherwise, fill in the last parabola segment
+        for i in range(last,len(samples)):
+            samples[i] = copysign(square(i - last), samples[i])
+            
     return samples
