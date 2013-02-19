f9560df24d03b55a7ed6f78e3c12315c1b7b3257
Matthew Sorensen <matthew.d.sorensen@gmail.com>
2013-02-18 16:03:55 -0800

    As we only write things once, no need to min

--- core/sdt.py
+++ core/sdt.py
@@ -5,23 +5,19 @@ from util import sgn
 def square(x):
     return x * x
 
-def spmin(sample,dt):
-    """ Returns a number such that sgn(spmin(x,y)) = sgn(x) and |spmin(x,y)| = min(|x|,|y|) """
-    mag = min(abs(sample),abs(dt))
-    return math.copysign(mag, sample)
 
 def reconstruct(coeff,size, old):
     minx, maxx = coeff
 
     if minx is None:
         for i in range(0,maxx):
-            old[i] = spmin(old[i], square(i - maxx))
+            old[i] = math.copysign(square(i - maxx), old[i])
     elif maxx is None:
         for i in range(minx,size):          
-            old[i] = spmin(old[i], square(i - minx))
+            old[i] = math.copysign(square(i - minx), old[i])
     else:
         for i in range(minx,maxx+1):
-            old[i] = spmin(old[i], min([square(i - minx), square(i - maxx)]))
+            old[i] = math.copysign(min([square(i - minx), square(i - maxx)]), old[i])
 
 def first_pass(samples):
     # we want to turn samples into a piece-wise set of quadratics
@@ -48,6 +44,3 @@ def first_pass(samples):
         reconstruct(c, ls, samples)
 
     return samples
-
-
-
