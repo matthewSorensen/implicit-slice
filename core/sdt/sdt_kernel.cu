fa28f2eb95a7b6f2a97c7ab15eed3004aa970306
Matthew Sorensen <matthew.d.sorensen@gmail.com>
2013-02-26 16:49:43 -0800

    change first pass to vertical to make second pass simple. kills performance, but will revert latter

--- core/sdt/sdt_kernel.cu
+++ core/sdt/sdt_kernel.cu
@@ -1,3 +1,6 @@
+
+texture<float,2, cudaReadModeElementType> tex;
+
 __device__ int sgn(const float f){
   if(f == 0.0)
     return 0;
@@ -25,25 +28,25 @@ __device__ float square(const float x){
 __global__ void implicit_first_pass(float* sample, const int width, const int height){
   const int x = blockIdx.x * blockDim.x + threadIdx.x;
   if(height <= x) return;
-  sample = &(sample[x * width]);
+  sample = &(sample[x]);
   
   int psgn = sgn(sample[0]);
   int last = -1;
 
-  for(int i = 0; i < width; i++){
-    int ssgn = sgn(sample[i]);
+  for(int i = 0; i < height; i++){
+    int ssgn = sgn(sample[width * i]);
 
     if(ssgn != psgn){
       int upper = i;
       if(psgn == 0)
 	upper--;   
       if(last == -1){
-	for(int x = 0; x < upper; x++){
-	  sample[x] = safe_copysign(square(x - upper), sample[x]);
+	for(int x = 0; x < height; x++){
+	  sample[width * x] = safe_copysign(square(x - upper), sample[width * x]);
 	}
       } else {
-	for(int x = last; x <= upper; x++){
-	  sample[x] = safe_copysign(square(fminf(x - last,upper - x)), sample[x]);
+	for(int x = last; x <= height; x++){
+	  sample[width * x] = safe_copysign(square(fminf(x - last,upper - x)), sample[width * x]);
 	}
       }
       last = upper;
@@ -54,38 +57,37 @@ __global__ void implicit_first_pass(float* sample, const int width, const int he
   }
   if(last == -1){
     float val = safe_copysign(square(fmaxf(width, height) * 2), sample[0]);
-    for(int i = 0; i < width; i++){
-      sample[i] = val;
+    for(int i = 0; i < height; i++){
+      sample[width * i] = val;
     }
   } else {
-    for(int i = last; i < width; i++){
-      sample[i] = safe_copysign(square(i - last), sample[i]);
+    for(int i = last; i < height; i++){
+      sample[width * i] = safe_copysign(square(i - last), sample[width * i]);
     }
   }
   
 }
 
 __global__ void voxel_first_pass(float* sample, const int width, const int height){
-  const int x = blockIdx.x * blockDim.x + threadIdx.x;
+ const int x = blockIdx.x * blockDim.x + threadIdx.x;
   if(height <= x) return;
-  sample = &(sample[x * width]);
-
+  sample = &(sample[x]);
+  
   int psgn = sgn(sample[0]);
   int last = -1;
 
-  for(int i = 0; i < width; i++){
-    int ssgn = sgn(sample[i]);
+  for(int i = 0; i < height; i++){
+    int ssgn = sgn(sample[width * i]);
 
     if(ssgn != psgn){
       int upper = i;
-     
       if(last == -1){
-	for(int x = 0; x < upper; x++){
-	  sample[x] = safe_copysign(square(x - upper), sample[x]);
+	for(int x = 0; x < height; x++){
+	  sample[width * x] = safe_copysign(square(x - upper), sample[width * x]);
 	}
       } else {
-	for(int x = last; x <= upper; x++){
-	  sample[x] = safe_copysign(square(fminf(x - last,upper - x)), sample[x]);
+	for(int x = last; x <= height; x++){
+	  sample[width * x] = safe_copysign(square(fminf(x - last,upper - x)), sample[width * x]);
 	}
       }
       last = upper;
@@ -94,27 +96,28 @@ __global__ void voxel_first_pass(float* sample, const int width, const int heigh
   }
   if(last == -1){
     float val = safe_copysign(square(fmaxf(width, height) * 2), sample[0]);
-    for(int i = 0; i < width; i++){
-      sample[i] = val;
+    for(int i = 0; i < height; i++){
+      sample[width * i] = val;
     }
   } else {
-    for(int i = last; i < width; i++){
-      sample[i] = safe_copysign(square(i - last), sample[i]);
+    for(int i = last; i < height; i++){
+      sample[width * i] = safe_copysign(square(i - last), sample[width * i]);
     }
   }
 }
 
-__global__ void second_pass(float* bounds,int* verts, float* out,int width, int height){
 
+__global__ void second_pass(float* twod, float* bounds,int* verts, float* out,int width, int height){
+  // const int x = blockIdx.x * blockDim.x + threadIdx.x;
+  // if(width <= x) return;
 }
 
+__global__ void sign_and_sqrt(float* signs, float* values, float* output, int width, int height){
+  const int x = blockIdx.x * blockDim.x + threadIdx.x;
+  const int y = blockIdx.y * blockDim.y + threadIdx.y;
+  if(width <= x || height <= y) return;
+  int i = x + width * y;
 
-//    second_pass(bounds,verts,drv.Out(sample), width, height, block = block, grid = (bls, 1))
-
-
-// sampled implicit => one-d signed, squared => rebind as texture (can we eliminate all copies?) => compute unsigned, squared
-
-
-__global__ void sign_and_sqrt(float* values, float* output, int width, int height){
+  output[i] = safe_copysign(signs[i],values[i]);
 }
 
