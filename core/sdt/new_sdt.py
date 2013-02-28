66103640599d915a03651b6e5b83b8279603c3df
Matthew Sorensen <matthew.d.sorensen@gmail.com>
2013-02-27 19:09:23 -0800

    bring the python implementation in-line with the cuda version

--- core/sdt/new_sdt.py
+++ core/sdt/new_sdt.py
@@ -12,7 +12,6 @@ def run_threads(data):
         while True:
             for thread in threads:
                 thread.next()
-            print verts
 
     except Exception as e:
         print e, data
@@ -22,70 +21,68 @@ def square(x):
 
 def go(i,data,verts,coeffs):
     l = len(data)
-
-    # Perform the first set of reductions.
-    otheri = i - 1 if i & 1 else i + 1
-    other = data[otheri]
-
+    # First we write the value that this thread starts with
+    # to the shared memory and synchronize with the other threads.
     out = data[i]
-
-    if out > other:
-        out = other + 1
-        verts[i] = otheri
-        coeffs[i] = other
+    coeffs[i] = out
+    yield
+    # Then perform the initial reduction step - each pair of threads
+    # computes the most local distance transform
+    otherindex = i ^ 1
+    otherdata  = coeffs[otherindex]
+    if otherdata < out:
+        coeffs[i] = otherdata + 1
+        verts[i] = otherindex
     else:
-        verts[i]  = i
-        coeffs[i] = out
+        verts[i] = i
 
     yield
-
-    j = i & ~1 # points to the bottom of this frame
-
-    mask = 1
-    size = 4
-
+    # Now start gradually building bigger and bigger chunks of local distance transforms
+    j = i & ~1 # points to the bottom of this chunk
+    mask = 3
     while l > 0: 
         l = l >> 1 # Yes, this means we have a logarithmic algorithm =)
-        out = min(out, square(i - verts[j]) + coeffs[j], square(i - verts[j+1]) + coeffs[j+1])
+
+        base = j & ~3;
+        dest = (j >> 1) & ~1
+        par = i & mask
         
-        yield
-        dest = (j>>1) & ~1
+        lowest = square(i - verts[base]) + coeffs[base]
+        low = square(i - verts[base + 1]) + coeffs[base + 1]
+        high = square(i - verts[base + 2]) + coeffs[base + 2]
+        highest = square(i - verts[base + 3]) + coeffs[base + 3]
 
-        # choose two threads to write the top and bottom of the new frame
-        if 0 == (i % size):
-            print "thread",i,"is merging bottom"
+        out = min(out,lowest,low,high,highest)
 
-            existingv = verts[j]
-            existingc = coeffs[j]
-            otherv = verts[j+2]
-            otherc = coeffs[j+2]
+        vertex = None
+        coefficient = None
 
-            if (square(i -otherv) + otherc) < (square(i -existingv) + existingc):
-                verts[dest] = otherv
-                coeffs[dest] = otherc
+        if par == 0:
+            if high < lowest:
+                vertex = verts[base + 2]
+                coefficient = coeffs[base + 2]
             else:
-                verts[dest] = existingv
-                coeffs[dest] = existingc
-
-        elif  (size-1) == (i % size):
-
-            print "thread",i,"is merging top"
-
-            existingv = verts[j-1]
-            existingc = coeffs[j-1]
-            otherv = verts[j+1]
-            otherc = coeffs[j+1]
-
-            if (square(i -otherv) + otherc) < (square(i -existingv) + existingc):
-                verts[dest+1] = otherv
-                coeffs[dest+1] = otherc
+                vertex = verts[base]
+                coefficient = coeffs[base]      
+        elif par == mask:
+            if low <  highest:
+                vertex = verts[base + 1]
+                coefficient = coeffs[base + 1]
             else:
-                verts[dest+1] = existingv
-                coeffs[dest+1] = existingc
-                        
+                vertex = verts[base + 3]
+                coefficient = coeffs[base + 3]
+                par = 1
+    
         yield
+        
+        if not vertex is None:
+            coeffs[dest + par] = coefficient;
+            verts[dest + par] = vertex;
+    
+        yield
+
         j = dest
-        size = size << 1
-        
+        mask = (mask << 1) + 1;    
+
     data[i] = out
     yield
