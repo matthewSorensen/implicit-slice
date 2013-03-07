e6aa49b72785fa68208228ea74dde34a1e90d5b9
Matthew Sorensen <matthew.d.sorensen@gmail.com>
2013-03-07 14:48:14 -0800

    add simple line simplification

--- core/polyline.py
+++ core/polyline.py
@@ -1,16 +1,23 @@
 import numpy as np
 import util
 import math
+import heapq
+import matplotlib.pyplot as plt
+import matplotlib.patches as patches
+def area(x,y):
+    a,b = x
+    c,d = y
+
+    return 0.5 * abs(a * d - b * c)
 
 class Polyline:
 
-    def __init__(self,a,b):
-        self.points = [a,b]
-        self.closed = False
-
-    def close(self):
-        self.closed = True
-        return self
+    def __init__(self,a,*b):
+        if b == ():
+            self.points = [x for x in a]
+        else:
+            self.points = [a]
+            self.points.extend(b)
 
     def reverse(self):
         self.points.reverse()
@@ -40,73 +47,87 @@ class Polyline:
 
         return unchanged
 
-def sign_changes(points):
-    changes = []
-    prev = util.sgn(points[0])
-    l = len(points)
-    for i, point in enumerate(points + [points[0]]):
-        if not util.sgn(point) == prev:
-            changes = changes + [((i -1) % l, i % l)]
-        prev = util.sgn(point)
-    return changes
+    def plot(self):
+        ax = plt.gca()
+        patch = patches.Polygon(self.points)
+        ax.add_patch(patch)
 
-def intersect_unitbox(point,normal):
-    """ Always returns two intersections """
+#        plt.plot(self.points)
 
-    tnear, tfar = -1 * util.infinity, util.infinity
-    x,y = point
-    dx,dy = normal
+    def simplify(self,threshold):
+        """ Implementation of Visvalingam's algorithm for line simplification """
+        l = len(self.points)
 
-    if dx == 0:
-        return -1 * y / dy, (1-y) / dy
+        neighbors = [(i-1,i+1) for i in range(l)]
+        neighbors[0] = None
+        neighbors[-1] = None
+        triangles = [0] * (l - 2)
+        for i in range(1,l-1):
+            here = self.points[i]
+            size = area(self.points[i-1] - here, self.points[i+1] - here)
+            triangles[i-1] = (size, i)
 
-    t1 = -1 * x / dx
-    t2 = (1-x) /dx
-    if t1 > t2:
-        t1, t2 = t2, t1
-    if t1 > tnear:
-        tnear = t1
-    if t2 < tfar:
-        tfar = t2
-    if dy == 0:
-        return tnear, tfar
-    t1 = -1 * y / dy
-    t2 = (1-y) /dy
-    if t1 > t2:
-        t1, t2 = t2, t1
-    if t1 > tnear:
-        tnear = t1
-    if t2 < tfar:
-        tfar = t2
-    return tnear, tfar
+        heapq.heapify(triangles)
+    
+        while [] != triangles:
+            size, i = heapq.heappop(triangles)
+            if size > threshold:
+                break
 
-def from_sdf_corners(region,sdf):        
-    grad = 0.5 * util.jhat * (sdf[1] - sdf[0] + sdf[2] - sdf[3])
-    grad += 0.5 * util.ihat * (sdf[2] - sdf[1] - sdf[3] - sdf[0])
-    grad = np.array([-1 * grad[1], grad[0]])    
+            entry = neighbors[i]
+            if entry is None:
+                continue
 
-    corners = list(region.corners())
-    center = region.center()
+            lower, upper = entry
+            
+            if neighbors[lower] is None or neighbors[upper] is None:
+                continue
+            lowerp = self.points[lower]
+            upperp = self.points[upper]
+            diff = lowerp - upperp
 
-    (starts, starte), (ends, ende) = sign_changes(sdf)[:2]
+            ll, lu = neighbors[lower]
+            
+            heapq.heappush(triangles, (area(self.points[ll] - lowerp, diff), lower))
 
-    theta = math.atan2(grad[1], grad[0])
-
-    half = [0.5 * util.jhat, util.jhat + 0.5 * util.ihat, util.ihat + 0.5 * util.jhat, 0.5 * util.ihat]
-
-    if starte == ends or starts == ende:
-        start = None
-        if theta < math.pi / 4 or (theta > math.pi and theta < math.pi * (7/8)):
-            start = half[ends]
-        else:
-            start = half[ende]
+            hl, hu = neighbors[upper]
         
-        t = sum(intersect_unitbox(start, grad))
-        end = t * grad + start + region.ll
-        return Polyline(start + region.ll, end)
-    else:
-        v = 0.5 * util.ones
-        tmin, tmax = intersect_unitbox(v, grad)
-        start = center  + tmin * grad
-        end = tmax * grad + center 
-        return Polyline(start,end)
+            heapq.heappush(triangles, (area(self.points[hu] - upperp, diff), upper))
+            
+            neighbors[i] = None
+            neighbors[lower] = (ll,upper)
+            neighbors[upper] = (lower,hu)
+
+        neighbors[0] = 0
+        neighbors[-1] = 0
+
+        self.points = [self.points[i] for i in range(l) if not neighbors[i] is None]
+
+
+def fast_simplify(points):    
+    """ Performs preliminary simplification """
+
+    start = points.next()
+    yield start
+    end = points.next()
+    dirx, diry = end - start
+    for p in points:
+        dx, dy = p - start
+        
+        if diry * dx != dy  * dirx:
+            yield end
+            start = end
+            end = p
+            dirx, diry = end - start
+        end = p
+    yield end
+
+def polylines_via_matplotlib(data,levels):
+    """ This is a completely horrific hack and needs to be expunged asap. """
+
+    plt.clf()
+    contour = plt.contour(data,levels = levels)
+    for x in contour.collections:
+        for y in x._paths:
+            yield Polyline(fast_simplify(iter(y.vertices)))
+
